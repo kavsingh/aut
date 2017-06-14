5476fadadf3647e780ff399ed29e9f649bfeb747
Kav Singh <flakisms@gmail.com>
2017-06-14 23:49:58 +1000

    bundle via webpack

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const isProduction = process.env.NODE_ENV === 'production'
+const fromRoot = path.resolve.bind(path, __dirname)
+const publicPath = '/'
+
+module.exports = {
+    entry: {
+        bundle: ['./src/main.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: fromRoot('dist'),
+        publicPath,
+    },
+    devtool: false,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: fromRoot('node_modules'),
+                use: [{ loader: 'babel-loader' }],
+            },
+        ],
+    },
+    plugins: [
+        new webpack.EnvironmentPlugin('NODE_ENV'),
+        isProduction && new webpack.optimize.OccurrenceOrderPlugin(true),
+        isProduction &&
+            new webpack.optimize.UglifyJsPlugin({ sourceMap: true }),
+    ].filter(Boolean),
+    resolve: {
+        modules: [fromRoot('src'), 'node_modules'],
+        extensions: ['.js'],
+    },
+}
