d07ceba93dec845c9a85d661ee939c150b97e983
Kav Singh <flakisms@gmail.com>
2017-09-17 21:36:33 +0200

    switch to .js babel config

---
+++ .babelrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: [
+    [
+      'env',
+      {
+        modules: false,
+        useBuiltIns: 'usage',
+        loose: true,
+        targets: {
+          browsers: ['last 2 versions']
+        }
+      },
+    ],
+  ],
+  env: {
+    test: {
+      plugins: ['transform-es2015-modules-commonjs'],
+    },
+  },
+}
