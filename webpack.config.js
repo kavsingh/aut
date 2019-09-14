6cf6a18de8b3f6fc8c42f0cd59e49125f63918e8
Kav Singh <flakisms@gmail.com>
2019-09-14 23:37:48 +0200

    update webpack / build

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
 const path = require('path')
-const webpack = require('webpack')
-const BabelMinifyPlugin = require('babel-minify-webpack-plugin')
-const { default: PrepackPlugin } = require('prepack-webpack-plugin')
-const prepackConfig = require('./.prepackrc')
+// TODO: fix prepack issues
+// const { default: PrepackPlugin } = require('prepack-webpack-plugin')
+// const prepackConfig = require('./prepack.config')
 
 const isProd = process.env.NODE_ENV === 'production'
-
 const fromRoot = path.resolve.bind(path, __dirname)
 const publicPath = '/'
 
 module.exports = {
+    mode: isProd ? 'production' : 'development',
     entry: {
         bundle: ['./src/main.js'],
     },
@@ -18,7 +17,6 @@ module.exports = {
         path: fromRoot('dist'),
         publicPath,
     },
-    devtool: false,
     module: {
         rules: [
             {
@@ -29,10 +27,8 @@ module.exports = {
         ],
     },
     plugins: [
-        new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
-        new webpack.optimize.ModuleConcatenationPlugin(),
-        isProd && new PrepackPlugin({ prepack: prepackConfig }),
-        isProd && new BabelMinifyPlugin(),
+        // TODO: fix prepack issues
+        // isProd && new PrepackPlugin({ prepack: prepackConfig }),
     ].filter(Boolean),
     resolve: {
         modules: [fromRoot('src'), 'node_modules'],
