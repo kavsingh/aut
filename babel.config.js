5163343607714662d7ec0738c05276136a6cf791
Kav Singh <flakisms@gmail.com>
2019-09-14 21:23:24 +0200

    update babel deps

---
+++ babel.config.js
@@ -0,0 +1,14 @@
+module.exports = ({ env }) => ({
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                loose: true,
+                shippedProposals: true,
+                ...(env('test')
+                    ? { modules: 'commonjs', useBuiltIns: false }
+                    : { modules: false, useBuiltIns: 'usage', corejs: 3 }),
+            },
+        ],
+    ],
+})
