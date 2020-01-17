561ae06535434c00d8fbcefa42e4ad04d269f679
Kav Singh <flakisms@gmail.com>
2020-01-17 11:19:10 +0100

    use single render pass for all canvases

--- src/renderers/renderer-2d.test.ts
+++ src/renderers/renderer-2d.test.ts
@@ -1,23 +1,17 @@
 import { createRenderer } from './renderer-2d'
 
-const mockCanvas = () => {
-	const context2d = Object.assign(new CanvasRenderingContext2D(), {
-		fillRect: jest.fn(),
-		clearRect: jest.fn(),
-	})
-	const canvas = Object.assign(document.createElement('canvas'), {
-		getContext: jest.fn((contextType: string) =>
-			contextType === '2d' ? context2d : null,
-		),
-	})
+const getDrawCalls = (canvas: HTMLCanvasElement, type?: string) => {
+	const allDrawCalls = (canvas.getContext('2d') as any).__getDrawCalls() as {
+		type: string
+	}[]
 
-	return { canvas, context2d }
+	return type ? allDrawCalls.filter(call => call.type === type) : allDrawCalls
 }
 
 describe('Renderer 2d', () => {
 	it('Should draw state to canvas context', () => {
-		const { canvas, context2d } = mockCanvas()
-		const render = createRenderer([canvas], {
+		const targetCanvas = document.createElement('canvas')
+		const render = createRenderer([targetCanvas], {
 			width: 10,
 			height: 10,
 			cellDim: 1,
@@ -28,24 +22,27 @@ describe('Renderer 2d', () => {
 			[0, 1],
 		])
 
-		const { fillStyle, clearRect, fillRect } = context2d
-		const fillCalls = fillRect.mock.calls
+		const drawCalls = getDrawCalls(targetCanvas)
+		const clearRectCalls = getDrawCalls(targetCanvas, 'clearRect')
+		const fillRectCalls = getDrawCalls(targetCanvas, 'fillRect')
 
-		expect(canvas).toEqual(expect.objectContaining({ width: 10, height: 10 }))
-		expect(fillStyle).toMatch(/^#(000000|000)$/)
-		expect(clearRect).toHaveBeenCalledTimes(1)
-
-		expect(fillCalls).toHaveLength(2)
+		expect(drawCalls[0].type).toBe('clearRect')
+		expect(clearRectCalls).toHaveLength(1)
+		expect(fillRectCalls).toHaveLength(2)
 		// First active, second row from bottom (state is 2 rows)
-		expect(fillCalls[0]).toEqual([0, 8, 1, 1])
+		expect(fillRectCalls[0]).toEqual(
+			expect.objectContaining({ props: { width: 1, height: 1, x: 0, y: 8 } }),
+		)
 		// Second active, first row from bottom, second column
 		// (last row drawn at bottom of height)
-		expect(fillCalls[1]).toEqual([1, 9, 1, 1])
+		expect(fillRectCalls[1]).toEqual(
+			expect.objectContaining({ props: { width: 1, height: 1, x: 1, y: 9 } }),
+		)
 	})
 
 	it('Should draw only visible rows from state', () => {
-		const { canvas, context2d } = mockCanvas()
-		const render = createRenderer([canvas], {
+		const targetCanvas = document.createElement('canvas')
+		const render = createRenderer([targetCanvas], {
 			width: 10,
 			height: 4,
 			cellDim: 2,
@@ -60,22 +57,23 @@ describe('Renderer 2d', () => {
 			[0, 1, 1],
 		])
 
-		const { clearRect, fillRect } = context2d
-		const fillCalls = fillRect.mock.calls
-
-		expect(clearRect).toHaveBeenCalledTimes(1)
+		const fillRectCalls = getDrawCalls(targetCanvas, 'fillRect')
 
 		// Draw only visible state
 		// height 4, cell height 2 => 2 rows visible
 		// last visible row has 2 adjacent active slots, so draw once
-		expect(fillCalls).toHaveLength(2)
-		expect(fillCalls[0]).toEqual([0, 0, 2, 2])
-		expect(fillCalls[1]).toEqual([2, 2, 4, 2])
+		expect(fillRectCalls).toHaveLength(2)
+		expect(fillRectCalls[0]).toEqual(
+			expect.objectContaining({ props: { width: 2, height: 2, x: 0, y: 0 } }),
+		)
+		expect(fillRectCalls[1]).toEqual(
+			expect.objectContaining({ props: { width: 4, height: 2, x: 2, y: 2 } }),
+		)
 	})
 
 	it('Should respect draw options', () => {
-		const { canvas, context2d } = mockCanvas()
-		const render = createRenderer([canvas], {
+		const targetCanvas = document.createElement('canvas')
+		const render = createRenderer([targetCanvas], {
 			width: 2,
 			height: 1,
 			cellDim: 1,
@@ -85,17 +83,18 @@ describe('Renderer 2d', () => {
 
 		render([[1, 0]])
 
-		const { fillStyle, fillRect } = context2d
-		const fillCalls = fillRect.mock.calls
+		const fillRectCalls = getDrawCalls(targetCanvas, 'fillRect')
 
-		expect(fillStyle).toBe('#f00')
-		expect(fillCalls).toHaveLength(1)
-		expect(fillCalls[0]).toEqual([1, 0, 1, 1])
+		expect(targetCanvas.getContext('2d')?.fillStyle).toBe('#f00')
+		expect(fillRectCalls).toHaveLength(1)
+		expect(fillRectCalls[0]).toEqual(
+			expect.objectContaining({ props: { width: 1, height: 1, x: 1, y: 0 } }),
+		)
 	})
 
 	it('Should not draw if no active cells', () => {
-		const { canvas, context2d } = mockCanvas()
-		const render = createRenderer([canvas], {
+		const targetCanvas = document.createElement('canvas')
+		const render = createRenderer([targetCanvas], {
 			width: 1,
 			height: 1,
 			cellDim: 1,
@@ -105,6 +104,31 @@ describe('Renderer 2d', () => {
 		render([[]])
 		render([])
 
-		expect(context2d.fillRect).not.toHaveBeenCalled()
+		expect(getDrawCalls(targetCanvas, 'fillRect')).toHaveLength(0)
+	})
+
+	it('Should only draw rows to one canvas and share the result', () => {
+		const sourceCanvas = document.createElement('canvas')
+		const secondaryCanvas = document.createElement('canvas')
+		const render = createRenderer([sourceCanvas, secondaryCanvas], {
+			width: 2,
+			height: 1,
+			cellDim: 1,
+		})
+
+		render([[1, 0]])
+
+		const sourceFillRectCalls = getDrawCalls(sourceCanvas, 'fillRect')
+		const secondaryFillRectCalls = getDrawCalls(secondaryCanvas, 'fillRect')
+		const secondaryDrawImageCalls = getDrawCalls(secondaryCanvas, 'drawImage')
+
+		expect(sourceFillRectCalls).toHaveLength(1)
+		expect(secondaryFillRectCalls).toHaveLength(0)
+		expect(secondaryDrawImageCalls).toHaveLength(1)
+		expect(secondaryDrawImageCalls[0]).toEqual(
+			expect.objectContaining({
+				props: expect.objectContaining({ img: sourceCanvas }),
+			}),
+		)
 	})
 })
--- src/renderers/renderer-2d.ts
+++ src/renderers/renderer-2d.ts
@@ -14,20 +14,27 @@ export const createRenderer: CanvasRendererFactory = (
 		fillMode = 'active',
 	},
 ) => {
-	const contexts = canvases
+	const [drawingCanvas, ...targetCanvases] = canvases
+	const drawingContext = drawingCanvas.getContext('2d')
+	const targetContexts = targetCanvases
 		.map(canvas => canvas.getContext('2d'))
 		.filter(is2dContext)
+
+	if (!is2dContext(drawingContext)) {
+		throw new Error('could not create drawing context')
+	}
 
 	const maxRows = Math.floor(height / cellDim)
 	const groupFillRanges = groupIndecesBy<number>(
 		eq(fillMode === 'inactive' ? 0 : 1),
 	)
 
-	const clear = () =>
-		contexts.forEach(context => {
+	const clear = () => {
+		;[drawingContext, ...targetContexts].forEach(context => {
 			context.clearRect(0, 0, width, height)
 			context.fillStyle = fillColor
 		})
+	}
 
 	const drawRow = (row: number[], yOffset: number) => {
 		const fillRanges = groupFillRanges(row)
@@ -38,18 +45,17 @@ export const createRenderer: CanvasRendererFactory = (
 			const end = last(current)
 
 			if (start !== undefined && end !== undefined) {
-				contexts.forEach(context => {
-					context.fillRect(
-						start * cellDim,
-						yOffset,
-						current.length * cellDim,
-						cellDim,
-					)
-				})
+				drawingContext.fillRect(
+					start * cellDim,
+					yOffset,
+					current.length * cellDim,
+					cellDim,
+				)
 			}
 		}
 	}
 
+	Object.assign(drawingCanvas, { width, height })
 	canvases.forEach(canvas => Object.assign(canvas, { width, height }))
 
 	return state => {
@@ -60,5 +66,7 @@ export const createRenderer: CanvasRendererFactory = (
 		for (let i = startIdx; i < state.length; i++) {
 			drawRow(state[i], height - (state.length - i) * cellDim)
 		}
+
+		targetContexts.forEach(context => context.drawImage(drawingCanvas, 0, 0))
 	}
 }
