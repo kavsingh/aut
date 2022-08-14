bbf1106fb0a163274e45545bed696268529943af
Kav Singh <flakisms@gmail.com>
2022-08-15 00:26:31 +0200

    allow component update type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,11 +4,12 @@ export type EvolutionRule = (a: number, b: number, c: number) => 0 | 1
 
 export type ComponentProps<P> = P & Record<never, never>
 
-export type ComponentApi = {
+export type ComponentApi<T = Record<never, never>> = {
 	el: HTMLElement | DocumentFragment
+	update?: (props: T) => Promise<void>
 	dispose?: () => Promise<void>
 }
 
 export type Component<P = Record<string, unknown>> = (
 	props: ComponentProps<P>,
-) => ComponentApi
+) => ComponentApi<P>
