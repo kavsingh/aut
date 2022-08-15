dea3c472198ce4f577e99389f949e29cd1549372
Kav Singh <flakisms@gmail.com>
2022-08-15 22:50:07 +0200

    typegen

--- src/screens/scrolls/scrolls.module.css.d.ts
+++ src/screens/scrolls/scrolls.module.css.d.ts
@@ -1,7 +1,7 @@
 export const __esModule: true;
 export const container: string;
-export const main: string;
 export const worlds: string;
 export const thumbnailsContainer: string;
+export const main: string;
 export const buttons: string;
 
