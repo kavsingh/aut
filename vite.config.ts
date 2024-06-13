9ee7ee78cf6d9cbae4d4da0107f1fb1f9e847582
Kav Singh <flakisms@gmail.com>
2024-06-14 00:59:14 +0300

    remove unused router, detect dead files

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from "vite"
 import checkerPlugin from "vite-plugin-checker"
+import deadFilePlugin from "vite-plugin-deadfile"
 import { createHtmlPlugin } from "vite-plugin-html"
 import { viteSingleFile } from "vite-plugin-singlefile"
 import solidPlugin from "vite-plugin-solid"
@@ -15,6 +16,7 @@ export default defineConfig(({ mode }) => {
 			checker(mode),
 			viteSingleFile(),
 			createHtmlPlugin(),
+			deadfiles(mode),
 		].filter(Boolean),
 		deps: { optimizer: { web: { include: ["vitest-canvas-mock"] } } },
 		test: {
@@ -26,6 +28,15 @@ export default defineConfig(({ mode }) => {
 		},
 	}
 })
+
+function deadfiles(mode: string) {
+	if (mode !== "production") return undefined
+
+	return deadFilePlugin({
+		root: "src",
+		exclude: ["**/__test__/**", "**/*.test.*", "**/*.d.ts"],
+	})
+}
 
 function checker(mode: string) {
 	if (mode !== "development") return undefined
