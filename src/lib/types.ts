a78a21648b824963643e19f54de91e5deac48596
Kav Singh <flakisms@gmail.com>
2024-08-07 02:10:43 +0200

    clean out some unused types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,3 @@
 export type WorldState = number[][]
 export type WorldStateEvolver = (world: WorldState) => WorldState
 export type EvolutionRule = (a: number, b: number, c: number) => 0 | 1
-
-export type ComponentProps<P> = P & Record<never, never>
-
-export type ComponentApi<T = Record<never, never>> = {
-	el: HTMLElement | DocumentFragment
-	update?: (props: T) => Promise<void>
-	dispose?: () => Promise<void>
-}
-
-export type Component<P = Record<string, unknown>> = (
-	props: ComponentProps<P>,
-) => ComponentApi<P>
