30e1ada0966fe66e5dfb1b3eda38409932439d82
Kav Singh <flakisms@gmail.com>
2024-09-16 01:21:35 +0200

    setup webgpu pipeline

--- src/pages/scrollsgpu/index.tsx
+++ src/pages/scrollsgpu/index.tsx
@@ -1,3 +1,33 @@
+/// <reference types="@webgpu/types" />
+
+import { onMount } from "solid-js"
+
+import { init } from "./renderer"
+
 export default function Scrollsgpu() {
-	return <div />
+	let canvasEl: HTMLCanvasElement | undefined
+
+	onMount(() => {
+		const context = canvasEl?.getContext("webgpu")
+
+		if (!context) return
+
+		void getDevice().then((device) => {
+			init(device, context)
+		})
+	})
+
+	return (
+		<div class="grid size-full place-items-center">
+			<canvas width="400" height="400" ref={(el) => (canvasEl = el)} />
+		</div>
+	)
+}
+
+async function getDevice() {
+	const adapter = await navigator.gpu.requestAdapter()
+
+	if (!adapter) return Promise.reject(new Error("no adapter"))
+
+	return adapter.requestDevice()
 }
