7e9babe93aadd0ec00f628878230e8787db09f55
Kav Singh <flakisms@gmail.com>
2024-09-16 00:09:09 +0200

    setup dev route

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,17 @@ import { Route, HashRouter } from "@solidjs/router"
 
 import Construct from "#pages/construct"
 import Scrolls from "#pages/scrolls"
+import Scrollsgpu from "#pages/scrollsgpu"
 
 import "./index.css"
+
 import AppLayout from "./layouts/app"
 
 export default function App() {
 	return (
 		<HashRouter root={AppLayout}>
 			<Route path="/" component={Scrolls} />
+			<Route path="/gpu" component={Scrollsgpu} />
 			<Route path="/construct" component={Construct} />
 		</HashRouter>
 	)
---
+++ src/pages/scrollsgpu/index.tsx
@@ -0,0 +1,3 @@
+export default function Scrollsgpu() {
+	return <div />
+}
